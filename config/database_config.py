e00c8b4266c30d3de20e8aafb3126d299d2893be
shuyx <18271801652@163.com>
2025-11-24 16:22:37 +0800

    自动更新: 2025-11-24 16:22:37

---
+++ config/database_config.py
@@ -0,0 +1,33 @@
+# 导入sqlalchemy框架中的各个工具
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# mysql数据库的连接URL
+MYSQL_DATABASE_URL = "mysql+pymysql://root:123456@localhost:33306/shuyx_db"
+
+# 创建数据库引擎myEngine
+myEngine = create_engine(MYSQL_DATABASE_URL,
+    pool_size=5,            # 连接池大小
+    pool_timeout=30,        # 池中没有线程最多等待的时间，否则报错
+    echo=False              # 是否在控制台打印相关语句等
+    )
+
+# 创建基类myBase
+myBase = declarative_base()
+
+# 创建会话对象mySession
+mySession = sessionmaker(autocommit=False, autoflush=False, bind=myEngine)
+
+# 使用上下文模块，封装session,实现session的自动提交，自动回滚，自动关闭
+@contextmanager
+def session_maker(session=mySession()):
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
---
+++ main.py
@@ -0,0 +1,21 @@
+# 导入FastAPI
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app='main:app',
+        port=39666,
+        reload=True
+    )
